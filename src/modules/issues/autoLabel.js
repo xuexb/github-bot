22f8f9c00975677fa4d873af9cbf298ab8525cb6
xuexb <fe.xiaowu@gmail.com>
2017-10-23 12:37:24 +0800

    feat: add label based on content

---
+++ src/modules/issues/autoLabel.js
@@ -0,0 +1,17 @@
+/**
+ * @file 自动根据创建的 issue 内标识创建对应 label
+ * @author xuexb <fe.xiaowu@gmail.com>
+ */
+
+const { addLabelsToIssue } = require('../../github')
+
+function autoAssign (on) {
+  on('issues_opened', ({ payload, repo }) => {
+    const label = (payload.issue.body.match(/<!--\s*label:\s*(.+?)\s*-->/) || [])[1]
+    if (label) {
+      addLabelsToIssue(payload, label)
+    }
+  })
+}
+
+module.exports = autoAssign
